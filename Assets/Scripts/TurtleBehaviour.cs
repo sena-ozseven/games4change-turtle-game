629034413a2acf0269d1d2556e12affb3496e5a3
sena <gulozseven@gmail.com>
2025-11-09 01:44:09 +0300

    games4change start

---
+++ Assets/Scripts/TurtleBehaviour.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class TurtleBehaviour : MonoBehaviour
+{
+    public Transform seaTarget;
+    public Transform lightTarget;
+    public float turtleSpeed;
+    public static bool isLightOn = false;
+    private Rigidbody rb;
+    private Transform currentTarget; //target to walk to: sea or light
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
