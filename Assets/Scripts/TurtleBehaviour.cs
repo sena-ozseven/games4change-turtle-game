478906b09fc542d0cb907a12b6cd9f11d8af226a
sena <gulozseven@gmail.com>
2025-11-09 05:17:12 +0300

    bug çözmek...

--- Assets/Scripts/TurtleBehaviour.cs
+++ Assets/Scripts/TurtleBehaviour.cs
@@ -4,19 +4,19 @@ public class TurtleBehaviour : MonoBehaviour
 {
     public Transform seaTarget;
     public Transform lightTarget;
+    
     public float turtleSpeed = 3f;
     public static bool isLightOn = false;
+
     private Rigidbody rb;
-    private Transform currentTarget; //target to walk to: sea or light
-    
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private Transform currentTarget;
+
     void Start()
     {
-         rb = GetComponent<Rigidbody>();
+        rb = GetComponent<Rigidbody>();
         turtleSpeed = Random.Range(1.5f, 4.5f);
     }
 
-    // Update is called once per fr
     void Update()
     {
         if (isLightOn)
@@ -31,18 +31,13 @@ public class TurtleBehaviour : MonoBehaviour
 
     void FixedUpdate()
     {
-        if (currentTarget == null) return; // Hedef yoksa hareket etme
+        if (currentTarget == null) return;
 
-        // Hedefe doğru bir yön vektörü hesapla
         Vector3 direction = (currentTarget.position - transform.position).normalized;
-        
-        // Y eksenindeki hareketi engelle ki kaplumbağa havalanmasın
         direction.y = 0;
 
-        // Hesaplanan yönde kuvvet uygula
         rb.MovePosition(transform.position + direction * turtleSpeed * Time.fixedDeltaTime);
 
-        // Kaplumbağanın yüzünü hedefe döndür
         if (direction != Vector3.zero)
         {
             transform.rotation = Quaternion.LookRotation(direction);
